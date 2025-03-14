edd38569dd13b4a12c1c6df7171e2372f2133721
Goh Min Wen, Ted <ilovefirebase4@gmail.com>
2025-03-14 10:34:04 +0800

    final

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,25 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors"; // Import the cors package
 import shoppingCartRoutes from "./routes/shoppingCartRoutes";
+import bodyParser from "body-parser";
 
-const app = express();
+require("dotenv").config();
+var app = express();
 
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Your frontend's origin
+  origin: true, // Your frontend's origin
   optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+  credentials: true, // Allow credentials to be sent
 };
 
 app.use(cors(corsOptions)); // Use the cors middleware with the specified options
+app.set("trust proxy", 1);
 
 mongoose
   .connect("mongodb://localhost:27017/online-shopping")
@@ -21,5 +27,7 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api/cart", shoppingCartRoutes);
-
+app.get("/", function (req, res, next) {
+  res.json({ success: "Express" });
+});
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
 import app from "./app";
+const { affinidiProvider } = require("@affinidi/passport-affinidi");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+  try {
+    await affinidiProvider(app, {
+      id: "affinidi",
+      issuer: process.env.AFFINIDI_ISSUER,
+      client_id: process.env.AFFINIDI_CLIENT_ID,
+      client_secret: process.env.AFFINIDI_CLIENT_SECRET,
+      redirect_uris: ["http://localhost:5173/auth/callback"],
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+}
+
+startServer();
